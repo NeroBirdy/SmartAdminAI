fe678db70cf7c9f2cf61c610bc6419edf806b9df
NeroBirdy <nirvanav.39@gmail.com>
2026-04-06 21:21:53 +0500

    fakeAPI

--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "../../prisma/generated/prisma/db1/client";
-// import { PrismaClient as FakeAPIClient } from "../../prisma/generated/prisma/db2/client";
+import { PrismaClient as FakeAPIClient } from "../../prisma/generated/prisma/db2/client";
 
 export { usePrisma };
 
@@ -9,20 +9,21 @@ const adapter = new PrismaMariaDb({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  database: process.env.DATABASE_NAME_1,
   connectionLimit: 5,
 });
 
-// const adapterFakeAPI = new PrismaMariaDb({
-//   host: process.env.DATABASE_HOST,
-//   user: process.env.DATABASE_USER,
-//   password: process.env.DATABASE_PASSWORD,
-//   database: process.env.DATABASE_NAME,
-//   connectionLimit: 5,
-// });
+const adapterFakeAPI = new PrismaMariaDb({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME_2,
+  connectionLimit: 5,
+});
+
 const prisma = new PrismaClient({ adapter });
 
-// const prismaFakeAPI = new PrismaClient({ adapterFakeAPI });
+const prismaFakeAPI = new FakeAPIClient({ adapter: adapterFakeAPI });
 
 const usePrisma = () => {
   return prisma;
